bfb824696a227a089050f3245cf0f73b307acfc7
harshadahivadkar22 <harshadahivadkar2202@gmail.com>
2024-11-27 15:08:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "../config/database connection.js";
 import router from "../routes/feedback.js";
+
 const app=express();
-const PORT =5000
+const PORT = process.env.PORT || 4000;
 
  app.use(express.json());
 dotenv.config();
@@ -11,9 +12,6 @@ dotenv.config();
 dbConnect();
 //mounting api routes
 app.use("/api/v1",router)
-
-
-
 
 
 app.listen(PORT,()=>{
